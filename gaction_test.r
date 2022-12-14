81ad71c2b053264f7aabcbcb64769451f5fad2fd
Owen O'Connor <46877624+oroconnor@users.noreply.github.com>
2022-12-14 15:54:08 -0500

    Create gaction_test.r

---
+++ gaction_test.r
@@ -0,0 +1,10 @@
+
+#Load library
+library(tidyverse)
+
+
+# create a new row of data, with todayd's date and urls number
+row <- data.frame(Sys.time())
+
+# append at the end of the csv the new data
+write_csv(row,paste0('/data/datetime_lister.csv'),append = T)   
